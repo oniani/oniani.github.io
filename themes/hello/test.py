e41fe34830d4f6b7aed96488c6dca630a2b15560
David Oniani <onianidavid@gmail.com>
2020-01-04 06:48:23 -0600

    Small changes

---
+++ themes/hello/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+"""
+Filename: test.py
+Date: 2020-01-04 06:48:08 AM
+Author: David Oniani
+E-mail: onianidavid@gmail.com
+
+License:
+    The code is licensed under GNU General Public License v3.0.
+    Please read the LICENSE file in this distribution for details
+    regarding the licensing of this code.
+
+Description:
+    This is a description of the file.
+"""
